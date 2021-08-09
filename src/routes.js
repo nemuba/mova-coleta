cb710986c1f5281426d9d975119158c6f549870f
Alef Ojeda de Oliveira <alef.oliveira@inovamind.com.br>
2021-08-09 08:11:00 -0300

    fix lazy load

--- src/routes.js
+++ src/routes.js
@@ -1,44 +1,45 @@
 import React from 'react';
+import { retry } from './services/functions';
 
-const Toaster = React.lazy(() => import('./views/notifications/toaster/Toaster'));
-const Tables = React.lazy(() => import('./views/base/tables/Tables'));
+const Toaster = React.lazy(() => retry(()=> import('./views/notifications/toaster/Toaster')));
+const Tables = React.lazy(() => retry(()=> import('./views/base/tables/Tables')));
 
-const Breadcrumbs = React.lazy(() => import('./views/base/breadcrumbs/Breadcrumbs'));
-const Cards = React.lazy(() => import('./views/base/cards/Cards'));
-const Carousels = React.lazy(() => import('./views/base/carousels/Carousels'));
-const Collapses = React.lazy(() => import('./views/base/collapses/Collapses'));
-const BasicForms = React.lazy(() => import('./views/base/forms/BasicForms'));
+const Breadcrumbs = React.lazy(() => retry(()=> import('./views/base/breadcrumbs/Breadcrumbs')));
+const Cards = React.lazy(() => retry(()=> import('./views/base/cards/Cards')));
+const Carousels = React.lazy(() => retry(()=> import('./views/base/carousels/Carousels')));
+const Collapses = React.lazy(() => retry(()=> import('./views/base/collapses/Collapses')));
+const BasicForms = React.lazy(() => retry(()=> import('./views/base/forms/BasicForms')));
 
-const Jumbotrons = React.lazy(() => import('./views/base/jumbotrons/Jumbotrons'));
-const ListGroups = React.lazy(() => import('./views/base/list-groups/ListGroups'));
-const Navbars = React.lazy(() => import('./views/base/navbars/Navbars'));
-const Navs = React.lazy(() => import('./views/base/navs/Navs'));
-const Paginations = React.lazy(() => import('./views/base/paginations/Pagnations'));
-const Popovers = React.lazy(() => import('./views/base/popovers/Popovers'));
-const ProgressBar = React.lazy(() => import('./views/base/progress-bar/ProgressBar'));
-const Switches = React.lazy(() => import('./views/base/switches/Switches'));
+const Jumbotrons = React.lazy(() => retry(()=> import('./views/base/jumbotrons/Jumbotrons')));
+const ListGroups = React.lazy(() => retry(()=> import('./views/base/list-groups/ListGroups')));
+const Navbars = React.lazy(() => retry(()=> import('./views/base/navbars/Navbars')));
+const Navs = React.lazy(() => retry(()=> import('./views/base/navs/Navs')));
+const Paginations = React.lazy(() => retry(()=> import('./views/base/paginations/Pagnations')));
+const Popovers = React.lazy(() => retry(()=> import('./views/base/popovers/Popovers')));
+const ProgressBar = React.lazy(() => retry(()=> import('./views/base/progress-bar/ProgressBar')));
+const Switches = React.lazy(() => retry(()=> import('./views/base/switches/Switches')));
 
-const Tabs = React.lazy(() => import('./views/base/tabs/Tabs'));
-const Tooltips = React.lazy(() => import('./views/base/tooltips/Tooltips'));
-const BrandButtons = React.lazy(() => import('./views/buttons/brand-buttons/BrandButtons'));
-const ButtonDropdowns = React.lazy(() => import('./views/buttons/button-dropdowns/ButtonDropdowns'));
-const ButtonGroups = React.lazy(() => import('./views/buttons/button-groups/ButtonGroups'));
-const Buttons = React.lazy(() => import('./views/buttons/buttons/Buttons'));
-const Charts = React.lazy(() => import('./views/charts/Charts'));
-const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'));
-const CoreUIIcons = React.lazy(() => import('./views/icons/coreui-icons/CoreUIIcons'));
-const Flags = React.lazy(() => import('./views/icons/flags/Flags'));
-const Brands = React.lazy(() => import('./views/icons/brands/Brands'));
-const Alerts = React.lazy(() => import('./views/notifications/alerts/Alerts'));
-const Badges = React.lazy(() => import('./views/notifications/badges/Badges'));
-const Modals = React.lazy(() => import('./views/notifications/modals/Modals'));
-const Colors = React.lazy(() => import('./views/theme/colors/Colors'));
-const Typography = React.lazy(() => import('./views/theme/typography/Typography'));
-const Widgets = React.lazy(() => import('./views/widgets/Widgets'));
-const Users = React.lazy(() => import('./views/users/Users'));
-const User = React.lazy(() => import('./views/users/User'));
-const NewUser = React.lazy(() => import('./views/users/NewUser'));
-const Profile = React.lazy(() => import('./views/pages/profile/Profile'));
+const Tabs = React.lazy(() => retry(()=> import('./views/base/tabs/Tabs')));
+const Tooltips = React.lazy(() => retry(()=> import('./views/base/tooltips/Tooltips')));
+const BrandButtons = React.lazy(() => retry(()=> import('./views/buttons/brand-buttons/BrandButtons')));
+const ButtonDropdowns = React.lazy(() => retry(()=> import('./views/buttons/button-dropdowns/ButtonDropdowns')));
+const ButtonGroups = React.lazy(() => retry(()=> import('./views/buttons/button-groups/ButtonGroups')));
+const Buttons = React.lazy(() => retry(()=> import('./views/buttons/buttons/Buttons')));
+const Charts = React.lazy(() => retry(()=> import('./views/charts/Charts')));
+const Dashboard = React.lazy(() => retry(()=> import('./views/dashboard/Dashboard')));
+const CoreUIIcons = React.lazy(() => retry(()=> import('./views/icons/coreui-icons/CoreUIIcons')));
+const Flags = React.lazy(() => retry(()=> import('./views/icons/flags/Flags')));
+const Brands = React.lazy(() => retry(()=> import('./views/icons/brands/Brands')));
+const Alerts = React.lazy(() => retry(()=> import('./views/notifications/alerts/Alerts')));
+const Badges = React.lazy(() => retry(()=> import('./views/notifications/badges/Badges')));
+const Modals = React.lazy(() => retry(()=> import('./views/notifications/modals/Modals')));
+const Colors = React.lazy(() => retry(()=> import('./views/theme/colors/Colors')));
+const Typography = React.lazy(() => retry(()=> import('./views/theme/typography/Typography')));
+const Widgets = React.lazy(() => retry(()=> import('./views/widgets/Widgets')));
+const Users = React.lazy(() => retry(()=> import('./views/users/Users')));
+const User = React.lazy(() => retry(()=> import('./views/users/User')));
+const NewUser = React.lazy(() => retry(()=> import('./views/users/NewUser')));
+const Profile = React.lazy(() => retry(()=> import('./views/pages/profile/Profile')));
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
--- src/services/functions.js
+++ src/services/functions.js
@@ -5,3 +5,22 @@ export function reject(obj, keys) {
     .reduce((res, o) => Object.assign(res, o), {});
 };
 
+export function retry(fn, retriesLeft = 5, interval = 1000) {
+  return new Promise((resolve, reject) => {
+    fn()
+      .then(resolve)
+      .catch((error) => {
+        setTimeout(() => {
+          if (retriesLeft === 1) {
+            // reject('maximum retries exceeded');
+            reject(error);
+            return;
+          }
+
+          // Passing on "reject" is the important part
+          retry(fn, interval, retriesLeft - 1).then(resolve, reject);
+        }, interval);
+      });
+  });
+}
+
